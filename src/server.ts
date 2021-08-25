3657a230c2e029df31766989a764bcb5cf99d09e
Arnaud Ferrand <arferrand@gmail.com>
2021-08-25 09:03:44 +0200

    improves TSness

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
-import express = require('express');
+import express, { Application, Request, Response } from 'express';
 
-const app = express();
-
+const app: Application = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.send('Hello World!');
 });
 
-app.get('/health', (req, res) => {
-  res.send('ok');
+app.get('/health', async (req: Request, res: Response): Promise<Response> => {
+  return res.send('ok');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+try {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error(`Error occured: ${error.message}`);
+}
